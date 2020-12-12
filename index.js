222c7ae2d75fe72895193a72cb3946bffb428fde
Anuragtech02 <apal895@gmail.com>
2020-12-12 17:59:52 +0530

    Changed btn colors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const handleSubmit = async (e) => {
   e.preventDefault();
   //   console.log(e);
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const subject = document.getElementById("subject").value;
-  const message = document.getElementById("message").value;
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const subject = document.getElementById("subject");
+  const message = document.getElementById("message");
 
-  console.log({ name, email, subject, message });
+  const btn = document.getElementById("submit-btn");
+  btn.style.backgroundColor = "grey";
+
+  // console.log({ name, email, subject, message });
 
   await fetch("https://aaiwayindia.com:5001/mail", {
     method: "POST",
@@ -14,12 +17,19 @@ const handleSubmit = async (e) => {
       "content-type": "application/json",
     },
     body: JSON.stringify({
-      name,
-      email,
-      subject,
-      msg: message,
+      name: name.value,
+      email: email.value,
+      subject: subject.value,
+      msg: message.value,
     }),
   })
-    .then((res) => alert("Succesfully Submitted :)"))
+    .then((res) => {
+      alert("Succesfully Submitted :)");
+      name.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+      btn.style.backgroundColor = "var(--primaryColor)";
+    })
     .catch((e) => alert("Something went wrong :("));
 };
